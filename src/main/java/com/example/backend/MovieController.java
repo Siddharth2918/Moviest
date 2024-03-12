4c38b1e230588eae53dc7c8d430de587529cd2d5
Siddharth S. Singh <91372571+Siddharth2918@users.noreply.github.com>
2024-03-12 18:15:19 +0530

    Add files via upload

---
+++ src/main/java/com/example/backend/MovieController.java
@@ -0,0 +1,21 @@
+package com.example.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService service;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+    }
+}
